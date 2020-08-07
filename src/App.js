5067f27444056b46f6941e1e67ab9c4f07c651ae
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-07 15:49:14 +0200

    slider section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/styles.css'
 import Header from './components/header_footer/Header'
+import Featured from './components/featured'
 
 function App() {
   return (
     <div style={{height: '1500px', backgroundColor: 'indigo'}}>
      <Header />
+     <Featured />
     </div>
   );
 }
