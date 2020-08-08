4a704c2fc9b44f21e1a3158537924ea5ca6c465e
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-09 00:11:35 +0200

    adding prices

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
 import VenueInfo from "./components/VenueInfo";
 import Highlights from "./components/Highlights";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Featured />
       <VenueInfo />
       <Highlights />
+      <Pricing />
     </div>
   );
 }
