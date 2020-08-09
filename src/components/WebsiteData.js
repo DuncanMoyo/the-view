6c32360316e7cf2c258525cf0b68d69fd2eb946a
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-09 12:25:03 +0200

    mapped slider images

---
+++ src/components/WebsiteData.js
@@ -0,0 +1,12 @@
+import SliderOne from '../assets/images/slide_one.jpg';
+import SliderTwo from '../assets/images/slide_two.jpg';
+import SliderThree from '../assets/images/slide_three.jpg';
+
+const websiteData = {
+  arianaImage: [
+    SliderOne, SliderTwo, SliderThree
+  ],
+
+}
+
+export default websiteData;
--- src/components/featured/Carousel.js
+++ src/components/featured/Carousel.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-import slide_1 from "../../assets/images/slide_one.jpg";
-import slide_2 from "../../assets/images/slide_two.jpg";
-import slide_3 from "../../assets/images/slide_three.jpg";
+import websiteData from "../WebsiteData";
 
 const Carousel = () => {
   const settings = {
@@ -11,40 +9,27 @@ const Carousel = () => {
     autoplay: true,
     speed: 500,
   };
+
+  const { arianaImage } = websiteData;
   return (
     <div
       className="carrousel_wrapper"
       style={{ overflow: "hidden", height: `${window.innerHeight}px` }}
     >
       <Slider {...settings}>
-        {/* will loop this later */}
-        <div>
-          <div
-            className="carrousel_image"
-            style={{
-              background: `url(${slide_1})`,
-              height: `${window.innerHeight}px`,
-            }}
-          ></div>
-        </div>
-        <div>
-          <div
-            className="carrousel_image"
-            style={{
-              background: `url(${slide_2})`,
-              height: `${window.innerHeight}px`,
-            }}
-          ></div>
-        </div>
-        <div>
-          <div
-            className="carrousel_image"
-            style={{
-              background: `url(${slide_3})`,
-              height: `${window.innerHeight}px`,
-            }}
-          ></div>
-        </div>
+        {arianaImage.map((arianaImage) => {
+          return (
+            <div>
+              <div
+                className="carrousel_image"
+                style={{
+                  background: `url(${arianaImage})`,
+                  height: `${window.innerHeight}px`,
+                }}
+              ></div>
+            </div>
+          );
+        })}
       </Slider>
     </div>
   );
