f88e4154b18151de9d557878f4f9e49099a1c3cd
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-09 10:57:44 +0200

    adding a scroll

--- src/App.js
+++ src/App.js
@@ -7,16 +7,27 @@ import Highlights from "./components/Highlights";
 import Pricing from "./components/Pricing";
 import Location from "./components/Location";
 import Footer from "./components/header_footer/Footer";
+import { Element } from "react-scroll";
 
 function App() {
   return (
     <div style={{ height: "1500px", backgroundColor: "indigo" }}>
       <Header />
-      <Featured />
-      <VenueInfo />
-      <Highlights />
-      <Pricing />
-      <Location />
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="venue_info">
+        <VenueInfo />
+      </Element>
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
       <Footer />
     </div>
   );
