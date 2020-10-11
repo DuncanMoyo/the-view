111a3c7c815ac3d61fefd1c2c3e64bf8bf216b72
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-11 23:04:48 +0200

    Updates

---
+++ precache-manifest.647e8ec40eb02dd36b88f77f013e224b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97ff1b41e4cf1720226e222da745683",
+    "url": "/the-view/index.html"
+  },
+  {
+    "revision": "c924aa7f6355a2b4df2a",
+    "url": "/the-view/static/css/main.afcb3cf0.chunk.css"
+  },
+  {
+    "revision": "9df92b3e7b1806105220",
+    "url": "/the-view/static/js/2.bd877503.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/the-view/static/js/2.bd877503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c924aa7f6355a2b4df2a",
+    "url": "/the-view/static/js/main.b6d9c771.chunk.js"
+  },
+  {
+    "revision": "b1fba5ec929725c574fa",
+    "url": "/the-view/static/js/runtime-main.97ede1b6.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/the-view/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/the-view/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/the-view/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
