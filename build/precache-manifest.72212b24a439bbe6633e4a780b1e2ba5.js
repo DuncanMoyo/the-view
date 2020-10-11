5c852f0bdda8495753ba6602e69b96096b81482a
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-11 22:39:51 +0200

    Github pages deploy

---
+++ build/precache-manifest.72212b24a439bbe6633e4a780b1e2ba5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36ec997852c301f08e8c351b17b1be9",
+    "url": "/http//:DuncanMoyo.github.io/the-view/index.html"
+  },
+  {
+    "revision": "c924aa7f6355a2b4df2a",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/css/main.afcb3cf0.chunk.css"
+  },
+  {
+    "revision": "9df92b3e7b1806105220",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/js/2.bd877503.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/js/2.bd877503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c924aa7f6355a2b4df2a",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/js/main.b6d9c771.chunk.js"
+  },
+  {
+    "revision": "e65b465350c24e9ced7b",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/js/runtime-main.6359bdae.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/http//:DuncanMoyo.github.io/the-view/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
